5a3133b74ad8c169b867e1e3bfbcc177f1507e4f
Ed Davey <ed.davey@digital.cabinet-office.gov.uk>
2025-08-20 15:33:50 +0100

    Add controllers

---
+++ app/controllers/admin/base_controller.rb
@@ -0,0 +1,66 @@
+class Admin::BaseController < ApplicationController
+  # include Admin::EditionRoutesHelper
+  # include PermissionsCheckerConcern
+
+  layout "design_system"
+  prepend_before_action :authenticate_user!
+
+  def limit_edition_access!
+    enforce_permission!(:see, @edition)
+  end
+
+  def require_fatality_handling_permission!
+    forbidden! unless current_user.can_handle_fatalities?
+  end
+
+  def enforce_permission!(action, subject)
+    unless can?(action, subject)
+      raise Whitehall::Authority::Errors::PermissionDenied.new(action, subject)
+    end
+  end
+
+  rescue_from Whitehall::Authority::Errors::PermissionDenied do |exception|
+    logger.warn "Attempt to perform '#{exception.action}' on #{exception.subject} prevented."
+    forbidden!
+  end
+
+  rescue_from Whitehall::Authority::Errors::InvalidAction do |exception|
+    logger.warn "Attempt to perform unknown action '#{exception.action}' prevented."
+    forbidden!
+  end
+
+  def prevent_modification_of_unmodifiable_edition
+    if @edition.unmodifiable?
+      alert = "You cannot modify a #{@edition.state} #{@edition.type.titleize}"
+      redirect_to admin_edition_path(@edition), alert:
+    end
+  end
+
+  def product_name
+    Whitehall.product_name
+  end
+  helper_method :product_name
+
+private
+
+  def forbidden!
+    prepend_view_path Rails.root.join("lib/engines/content_block_manager/app/views") if request.path.start_with?(ContentBlockManager.router_prefix)
+    render "admin/errors/forbidden", status: :forbidden
+  end
+
+  def typecast_for_attachable_routing(attachable)
+    case attachable
+    when Edition then attachable.becomes(Edition)
+    when ConsultationResponse then attachable.becomes(ConsultationResponse)
+    when CallForEvidenceResponse then attachable.becomes(CallForEvidenceResponse)
+    else attachable
+    end
+  end
+  helper_method :typecast_for_attachable_routing
+
+  # Override the default Rails behaviour to raise an exception when receiving
+  # unverified requests instead of nullifying the session
+  def handle_unverified_request
+    raise ActionController::InvalidAuthenticityToken
+  end
+end
---
+++ app/controllers/content_block_manager/base_controller.rb
@@ -0,0 +1,47 @@
+class ContentBlockManager::BaseController < Admin::BaseController
+  before_action :check_block_manager_permissions, :set_sentry_tags
+
+  def check_block_manager_permissions
+    forbidden! unless current_user.gds_admin?
+  end
+
+  def scheduled_publication_params
+    params.require(:scheduled_at).permit("scheduled_publication(1i)",
+                                         "scheduled_publication(2i)",
+                                         "scheduled_publication(3i)",
+                                         "scheduled_publication(4i)",
+                                         "scheduled_publication(5i)")
+  end
+
+  def edition_params
+    params.require("content_block/edition")
+          .permit(
+            :organisation_id,
+            :creator,
+            :instructions_to_publishers,
+            "scheduled_publication(1i)",
+            "scheduled_publication(2i)",
+            "scheduled_publication(3i)",
+            "scheduled_publication(4i)",
+            "scheduled_publication(5i)",
+            :title,
+            :internal_change_note,
+            :change_note,
+            :major_change,
+            document_attributes: %w[block_type],
+            details: @schema.permitted_params,
+          )
+          .merge!(creator: current_user)
+  end
+
+  def set_sentry_tags
+    Sentry.set_tags(engine: "content_block_manager")
+  end
+
+  def product_name
+    "Content Block Manager"
+  end
+
+  delegate :support_url, to: :ContentBlockManager
+  helper_method :support_url
+end
