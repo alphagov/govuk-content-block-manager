69c67528692f045e009c39cd015ca3ddcca04549
Ed Davey <ed.davey@digital.cabinet-office.gov.uk>
2025-08-20 14:50:25 +0100

    Add cucumber testing with a single feature
    This commit sets up cucumber testing and brings in a single
    "feature" initially "Create 'Contact' object". This is
    probably the most complex test we have so will allow us to
    drive out the maximum amount of complexity in a single
    "outside -> in" process. By this we mean running the highest
    level test and then filling in lower level tests and
    implementation.

---
+++ features/support/publishing_api.rb
@@ -0,0 +1,24 @@
+require "gds_api/publishing_api"
+require "gds_api/test_helpers/publishing_api"
+require_relative "mocha"
+
+Before do
+  publishing_api_v1_endpoint = File.join(
+    Plek.find("publishing-api"),
+    "publish-intent",
+  )
+
+  stub_request(:any, %r{\A#{publishing_api_v1_endpoint}})
+  GdsApi::PublishingApi.any_instance.stubs(:discard_draft)
+  GdsApi::PublishingApi.any_instance.stubs(:publish)
+  GdsApi::PublishingApi.any_instance.stubs(:put_content)
+  GdsApi::PublishingApi.any_instance.stubs(:patch_links)
+  GdsApi::PublishingApi.any_instance.stubs(:unpublish)
+  GdsApi::PublishingApi.any_instance.stubs(:get_events_for_content_id).returns([])
+  stub_request(:get, %r{\A#{Plek.find('publishing-api')}/v2/links})
+    .to_return(body: { links: {} }.to_json)
+  stub_request(:get, %r{\A#{Plek.find('publishing-api')}/v2/expanded-links})
+    .to_return(body: { expanded_links: {} }.to_json)
+end
+
+World(GdsApi::TestHelpers::PublishingApi)
