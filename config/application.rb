56fa2450557e33480c9bdcfdb2336094bfe8ef87
Ed Davey <ed.davey@digital.cabinet-office.gov.uk>
2025-08-20 15:32:19 +0100

    Tweak application main file to match Whitehall

--- config/application.rb
+++ config/application.rb
@@ -7,13 +7,13 @@ require "active_job/railtie"
 require "active_record/railtie"
 # require "active_storage/engine"
 require "action_controller/railtie"
-# require "action_mailer/railtie"
+require "action_mailer/railtie"
 # require "action_mailbox/engine"
 # require "action_text/engine"
 require "action_view/railtie"
 require "sprockets/railtie"
-require "action_cable/engine"
-# require "rails/test_unit/railtie"
+# require "action_cable/engine"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -35,7 +35,10 @@ module GovukContentBlockManager
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+
+    config.eager_load_paths += %W[
+      #{config.root}/lib
+    ]
 
     # Don't generate system test files.
     config.generators.system_tests = nil
