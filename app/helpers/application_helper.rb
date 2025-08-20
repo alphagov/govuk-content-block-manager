47064d4d6739e19fc04c04a3d99d0cfd277eba3a
Ed Davey <ed.davey@digital.cabinet-office.gov.uk>
2025-08-20 15:26:11 +0100

    Add to main helper file
    1. include RecordTagHelper, used to create wrapper elements
       in markup
    
    2. copy over the `get_content_id(edition)` method used in
       layout template

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
+require "record_tag_helper/helper"
+
 module ApplicationHelper
+  include ActionView::Helpers::RecordTagHelper
+
+  def get_content_id(edition)
+    return if edition.nil?
+
+    return unless edition.respond_to?("content_id")
+
+    edition.content_id
+  end
 end
