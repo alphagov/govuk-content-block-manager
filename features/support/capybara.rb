7f54c851616e32588367728f271666494e52797e
Ed Davey <ed.davey@digital.cabinet-office.gov.uk>
2025-08-20 14:50:25 +0100

    Add cucumber testing with a single feature
    This commit sets up cucumber testing and brings in a single
    "feature" initially "Create 'Contact' object". This is
    probably the most complex test we have so will allow us to
    drive out the maximum amount of complexity in a single
    "outside -> in" process. By this we mean running the highest
    level test and then filling in lower level tests and
    implementation.

---
+++ features/support/capybara.rb
@@ -0,0 +1,36 @@
+# On the CI box we have seen intermittent failures.
+# We think this may be due to timeouts (the default is 2 secs),
+# so we've increased the default timeout.
+Capybara.default_max_wait_time = 5
+
+# Allow Capybara to click a <label> even if its corresponding <input> isn't visible on screen.
+# This needs to be enabled when using custom-styled checkboxes and radios, such as those
+# in the GOV.UK Design System.
+Capybara.automatic_label_click = true
+
+Capybara.register_driver :playwright do |app|
+  Capybara::Playwright::Driver.new(app,
+                                   browser_type: :chromium,
+                                   headless: true)
+end
+
+Capybara.configure do |config|
+  config.asset_host = ENV.fetch("ASSET_HOST", nil)
+end
+
+Capybara.javascript_driver = :playwright
+
+module ScreenshotHelper
+  def screenshot(name = "capybara")
+    path = Rails.root.join("tmp/#{name}.png")
+    if Capybara.current_session.driver.is_a?(Capybara::Playwright::Driver)
+      Capybara.current_session.driver.with_playwright_page do |page|
+        page.screenshot(path:, fullPage: true)
+      end
+    else
+      page.driver.render(path, full: true)
+    end
+  end
+end
+
+World(ScreenshotHelper)
