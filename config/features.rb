5b8d0912167949d4327b05a1f005c4c5759701a7
Ed Davey <ed.davey@digital.cabinet-office.gov.uk>
2025-08-21 15:26:43 +0100

    Add FlipFlop config for feature flagging

---
+++ config/features.rb
@@ -0,0 +1,33 @@
+Flipflop.configure do
+  # Strategies will be used in the order listed here.
+  strategy :active_record, hidden: !Rails.env.development?
+  strategy :cookie
+  strategy :default
+
+  # Other strategies:
+  #
+  # strategy :sequel
+  # strategy :redis
+  #
+  # strategy :query_string
+  # strategy :session
+  #
+  # strategy :my_strategy do |feature|
+  #   # ... your custom code here; return true/false/nil.
+  # end
+
+  # Declare your features, e.g:
+  #
+  # feature :world_domination,
+  #   default: true,
+  #   description: "Take over the world."
+  feature :maintenance_mode, description: "Put Whitehall into maintenance mode for planned downtime", default: false
+  feature :govspeak_visual_editor, description: "Enables a visual editor for Govspeak fields", default: false
+  feature :show_link_to_content_block_manager, description: "Shows link to Content Block Manager from Whitehall editor", default: Whitehall.integration_or_staging?
+  feature :show_all_content_block_types,
+          description: "Show all applicable content block types in Content Block Manager",
+          default: Whitehall.integration_or_staging? || !Rails.env.production?
+  feature :use_friendly_embed_codes,
+          description: "Use embed codes with friendly IDs in Content Block Manager",
+          default: Whitehall.integration_or_staging? || !Rails.env.production?
+end
