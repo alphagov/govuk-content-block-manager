e4f081b2f55fea39d7f378439237e5502ca01447
pezholio <pezholio@gmail.com>
2025-08-26 14:22:48 +0100

    Add tests for `SearchableByKeyword`
    The code had some queries specific to MySql, so we’re adding the
    `pg_search` gem to support rudimentary fulltext search. As users
    begin to use the search, we can tweak as necessary.

--- app/models/content_block_manager/content_block/document/scopes/searchable_by_keyword.rb
+++ app/models/content_block_manager/content_block/document/scopes/searchable_by_keyword.rb
@@ -2,25 +2,11 @@ module ContentBlockManager
   module ContentBlock::Document::Scopes::SearchableByKeyword
     extend ActiveSupport::Concern
 
-    SQL = <<-SQL.freeze
-        MATCH(
-           content_block_editions.title,#{' '}
-           content_block_editions.details_for_indexing,#{' '}
-           content_block_editions.instructions_to_publishers
-        ) AGAINST (:pattern IN BOOLEAN MODE)
-    SQL
-
     included do
-      scope :with_keyword,
-            lambda { |keywords|
-              split_keywords = keywords.split
-              pattern = split_keywords.map { |k|
-                escaped_word = Regexp.escape(k)
-                "+#{escaped_word}"
-              }.join(" ")
-              joins(:latest_edition)
-                .where(SQL, pattern:)
-            }
+      include PgSearch::Model
+      pg_search_scope :with_keyword, associated_against: {
+        editions: %i[title details instructions_to_publishers],
+      }
     end
   end
 end
