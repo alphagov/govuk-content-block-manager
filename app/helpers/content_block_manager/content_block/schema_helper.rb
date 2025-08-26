6006f3ec39015a3f77cd99c339a5e17164a13f78
pezholio <pezholio@gmail.com>
2025-08-26 15:15:46 +0100

    Add schema helper tests
    This also uses the correct url helpers

--- app/helpers/content_block_manager/content_block/schema_helper.rb
+++ app/helpers/content_block_manager/content_block/schema_helper.rb
@@ -11,6 +11,6 @@ module ContentBlockManager::ContentBlock::SchemaHelper
 
   def redirect_url_for_subschema(subschema, content_block_edition)
     step = subschema.group.present? ? "#{Workflow::Step::GROUP_PREFIX}#{subschema.group}" : "#{Workflow::Step::SUBSCHEMA_PREFIX}#{subschema.id}"
-    content_block_manager.content_block_manager_content_block_workflow_path(content_block_edition, step:)
+    content_block_manager_content_block_workflow_path(content_block_edition, step:)
   end
 end
