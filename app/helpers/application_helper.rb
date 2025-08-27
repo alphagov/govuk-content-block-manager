c13851d7ac461876b5c9723f29c3dd266662c317
pezholio <pezholio@gmail.com>
2025-08-27 12:54:03 +0100

    Import application helper methods
    This imports a couple of methods from Whitehall that are used in
    our components.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,21 @@ module ApplicationHelper
 
     edition.content_id
   end
+
+  def linked_author(author, link_options = {})
+    if author
+      link_to(author.name, content_block_manager_user_path(author.uid), link_options)
+    else
+      "-"
+    end
+  end
+
+  def add_indefinite_article(noun)
+    indefinite_article = starts_with_vowel?(noun) ? "an" : "a"
+    "#{indefinite_article} #{noun}"
+  end
+
+  def starts_with_vowel?(word_or_phrase)
+    "aeiou".include?(word_or_phrase.downcase[0])
+  end
 end
---
+++ test/unit/app/helpers/application_helper_test.rb
@@ -0,0 +1,47 @@
+require "test_helper"
+
+class ApplicationHelperTest < ActionView::TestCase
+  extend Minitest::Spec::DSL
+
+  describe "#add_indefinite_article" do
+    it "prepends word with 'an' when a word starts with a vowel" do
+      %w[
+        apple
+        egg
+        igloo
+        office
+        unlikely
+      ].each do |word|
+        assert_equal add_indefinite_article(word), "an #{word}"
+      end
+    end
+
+    it "prepends word with 'a' when a word does not start with a vowel" do
+      %w[
+        bike
+        car
+        dog
+        flag
+        goat
+      ].each do |word|
+        assert_equal add_indefinite_article(word), "a #{word}"
+      end
+    end
+  end
+
+  describe "#linked_author" do
+    let(:user) { build(:user) }
+
+    it "links to an author if set" do
+      assert_equal linked_author(user), link_to(user.name, content_block_manager_user_path(user.uid), {})
+    end
+
+    it "passes link options to link_to" do
+      assert_equal linked_author(user, { class: "my-link" }), link_to(user.name, content_block_manager_user_path(user.uid), { class: "my-link" })
+    end
+
+    it "returns a dash if user is not set" do
+      assert_equal linked_author(nil), "-"
+    end
+  end
+end
