83031e8a72e012a2d53c0d9d1272b44e047dd22f
pezholio <pezholio@gmail.com>
2025-08-27 14:22:16 +0100

    Add helper methods to `ContentBlockManager` module

--- lib/content_block_manager.rb
+++ lib/content_block_manager.rb
@@ -1,6 +1,18 @@
 require_relative "./locale"
 
 module ContentBlockManager
+  def self.product_name
+    "Content Block Manager"
+  end
+
+  def self.support_url
+    "#{Plek.external_url_for('support')}/general_request/new"
+  end
+
+  def self.support_email_address
+    "feedback-content-modelling@digital.cabinet-office.gov.uk"
+  end
+
   def self.integration_or_staging?
     website_root = ENV.fetch("GOVUK_WEBSITE_ROOT", "")
     %w[integration staging].any? { |environment| website_root.include?(environment) }
