4a49e71914c220672df8778f73f735942a448a5c
pezholio <pezholio@gmail.com>
2025-08-27 15:35:28 +0100

    Import missing component JS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,10 @@
 //= require govuk_publishing_components/dependencies
+//= require govuk_publishing_components/components/accordion
 //= require govuk_publishing_components/components/add-another
+//= require govuk_publishing_components/components/checkboxes
 //= require govuk_publishing_components/components/copy-to-clipboard
 //= require govuk_publishing_components/components/govspeak
+//= require govuk_publishing_components/components/tabs
 //= require govuk_publishing_components/lib/cookie-functions
 //= require govuk_publishing_components/lib/trigger-event
 
