65f5153dc8020183dd0786fc8845eb2a221efca7
pezholio <pezholio@gmail.com>
2025-08-28 14:01:07 +0100

    Add a `lead_organisation_id` to the edition table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_28_125603) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_28_130017) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -50,6 +50,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_28_125603) do
     t.boolean "major_change"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.uuid "lead_organisation_id"
     t.index ["document_id"], name: "index_content_block_editions_on_document_id"
     t.index ["title"], name: "index_content_block_editions_on_title"
   end
