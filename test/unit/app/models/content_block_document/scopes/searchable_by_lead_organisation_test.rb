1aa53ae6831dce249ebdc9adb2454ba1f1dc65cf
pezholio <pezholio@gmail.com>
2025-08-28 16:01:51 +0100

    Update `SearchableByLeadOrganisation`
    We no longer need to join, and can just query by the ID

--- test/unit/app/models/content_block_document/scopes/searchable_by_lead_organisation_test.rb
+++ test/unit/app/models/content_block_document/scopes/searchable_by_lead_organisation_test.rb
@@ -4,17 +4,17 @@ class ContentBlockManager::SearchableByLeadOrganisationTest < ActiveSupport::Tes
   extend Minitest::Spec::DSL
 
   describe ".with_lead_organisation" do
-    test "finds documents with lead organisation on latest edition" do
-      matching_organisation = create(:organisation, id: "1234")
+    it "finds documents with lead organisation on latest edition" do
+      matching_organisation = build(:organisation)
       document_with_org = create(:content_block_document, :pension)
       _edition_with_org = create(:content_block_edition,
                                  :pension,
                                  document: document_with_org,
-                                 organisation: matching_organisation)
+                                 lead_organisation_id: matching_organisation.id)
       document_without_org = create(:content_block_document, :pension)
       _edition_without_org = create(:content_block_edition, :pension, document: document_without_org)
       _document_without_latest_edition = create(:content_block_document, :pension)
-      assert_equal [document_with_org], ContentBlockManager::ContentBlock::Document.with_lead_organisation("1234")
+      assert_equal [document_with_org], ContentBlockManager::ContentBlock::Document.with_lead_organisation(matching_organisation.id)
     end
   end
 end
